a04b1491527355579e841d6c8299115753054811
agusnarvaez <agus.narvaez@outlook.com>
2024-02-14 00:52:12 -0300

    Corrijo ruteo y lo paso a main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { Suspense, lazy } from 'react'
+import {RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const Home = lazy(() => import('./pages/Home/Home'))
+const About = lazy(() => import('./pages/About/About'))
+const Contact = lazy(() => import('./pages/Contact/Contact'))
+const Services = lazy(() => import('./pages/Services/Services'))
+const NotFound = lazy(() => import('./pages/NotFound/NotFound'))
+
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: 'about', element: <About /> },
+  { path: 'services', element: <Services /> },
+  { path: 'contact', element: <Contact /> },
+  { path: '*', element: <NotFound /> }
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <RouterProvider router={router} />
+    </Suspense>
   </React.StrictMode>,
 )
