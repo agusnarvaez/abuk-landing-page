ec916747297f7a6e0e774a8be568669157943701
agusnarvaez <agus.narvaez@outlook.com>
2024-02-19 01:55:58 -0300

    Agrego animación a texto de ABUK

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,12 @@ import './home.css'
 export default function Home() {
   return (
     <main className="page-container home-page">
-      <h1>ABUK</h1>
+      <h2>
+        <span className='letter letter-A'>A</span>
+        <span className='letter letter-B'>B</span>
+        <span className='letter letter-U'>U</span>
+        <span className='letter letter-K'>K</span>
+      </h2>
     </main>
   )
 }
