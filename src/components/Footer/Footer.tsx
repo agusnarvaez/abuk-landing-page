839ca3c9cf101d65f6a54eaec289a6e37fe5bbbd
agusnarvaez <agus.narvaez@outlook.com>
2024-02-19 00:04:45 -0300

    Agrego img svg de logo bapps

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 import './footer.css'
-import logoBapps from 'src/assets/logos/logo-bapps-footer.webp'
+import logoBapps from 'src/assets/logos/logo-bapps-footer.svg'
 interface FooterProps {
   theme: string
 }
